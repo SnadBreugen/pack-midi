c7ac2e19f717886391e010af7ae062856f6d4fb0
SnadBreugen <ralf.noetzel@audiotool.com>
2026-03-28 12:02:54 +0100

    Updates

--- assets/__vite-browser-external-C9iVVtcT.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-CaKHawV4.js";var t=e(((e,t)=>{t.exports={}}));export{t};
---
+++ assets/__vite-browser-external-e7-LdnMD.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-BAwFOXlG.js";var t=e(((e,t)=>{t.exports={}}));export{t};
